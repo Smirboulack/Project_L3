311cf441547fa465acfc3f4ff4d583da55172211
p1707160 <koureich.temirboulatov@etu.univ-lyon1.fr>
2023-01-23 23:15:17 +0100

    amelioration_html/css

---
+++ sql/script.sql
@@ -0,0 +1,10 @@
+create table utilisateurs(
+id_u INTEGER(5) NOT NULL AUTO_INCREMENT,
+pseudo_u VARCHAR(30),
+mot_de_passe VARCHAR(30),
+email VARCHAR(50),
+date_naiss date,
+primary key (id_u,pseudo_u,email)
+);
+
+alter table utilisateurs add unique (email);
--- static/menu.php
+++ static/menu.php
@@ -7,21 +7,23 @@
             <i class="fas fa-bars ouvrir"></i>
             <i class="fas fa-times fermer"></i>
         </div>
-        <ul class="menu">
+        <div class="toggle_image">
+        <a href="https://www.univ-lyon1.fr/" target="_blank"><img style="width:50%;height:20%;" src="images/lyon1.png" alt="image-lien" /></a>
+        </div>
+         <ul class="menu">
             <li><a href="index.php?page=accueil">Accueil</a></li>
-            <li><a href="index.php?page=legales">About</a></li>
+            <li><a href="about.php">About</a></li>
             <li><a href="index.php?page=jeu">Jouer</a></li>
             <li><a href="http://cazabetremy.fr/wiki/doku.php?id=projet:presentation#enseignants" target="_blank">Page
                     web de
                     l'UE</a></li>
             <?php if (!isset($_SESSION["logged"])) {
-            echo '<li><button class="btn" style="color: black;"><a href="index.php?page=inscription">Inscription</a></button></li>' .
-                '<li><button class="btn btn-secondary"><a href="index.php?page=connexion">Connexion</a></button></li>';
-        } else {
-            echo '<li><form method="POST"><input style="background-color:red;" type="submit" value="Se déconnecter" name="déconnexion""/></form></li>';
-        }
-        ?>
+                echo '<li><button class="btn" style="color: black;"><a href="index.php?page=inscription">Inscription</a></button></li>' .
+                    '<li><button class="btn btn-secondary"><a href="index.php?page=connexion">Connexion</a></button></li>';
+            } else {
+                echo '<li><form method="POST"><input style="background-color:red;" type="submit" value="Se déconnecter" name="déconnexion""/></form></li>';
+            }
+            ?>
         </ul>
     </nav>
 </header>
-<a href="https://www.univ-lyon1.fr/" target="_blank" style="position:fixed; top:0; right:800px;"><img src="images/lyon1.png" alt="image-lien" width="100%" height="50" /></a>
