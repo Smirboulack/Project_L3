4066a60bd9eab0b5dd118baffd742a5f2a979b00
p1707160 <koureich.temirboulatov@etu.univ-lyon1.fr>
2023-02-06 11:47:13 +0100

    Chat#2

--- controleurs/chat_controleur.php
+++ controleurs/chat_controleur.php
@@ -1,10 +1,12 @@
 <?php
-echo '<script src="javascript/users.js"></script>';
-echo '<link rel="stylesheet" href="css/chat.css">';
 
 if (!isset($_SESSION['logged'])) {
     header('Location: index.php?page=connexion');
 }else{
+    
+echo '<script src="script/users.js"></script>';
+echo '<link rel="stylesheet" href="css/chat.css">';
+
     $_SESSION["acces_chat"] = "ok";
     $_SESSION["userid"] = getUser_ID($_COOKIE['pseudo'], $connexion);
 }
